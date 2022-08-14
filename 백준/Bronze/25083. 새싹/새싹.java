c22b3e643f8ac0df110dfeffa3a672e1d91ba85e
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-15 00:46:54 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		output.append("         ,r'\"7").append("\n").append("r`-_   ,'  ,/").append("\n").append(" \\. \". L_r'").append("\n").append("   `~\\/").append("\n").append("      |").append("\n").append("      |");
+
+		System.out.println(output);
+
+	}
+	
+
+}
