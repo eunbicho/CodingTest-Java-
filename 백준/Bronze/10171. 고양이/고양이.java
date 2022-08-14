721b15420096ab5f40fc6eabd56908511d9ef94a
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-15 00:48:31 +0900

    [Bronze V] Title: 고양이, Time: 76 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		output.append("\\    /\\").append("\n").append(" )  ( ')").append("\n").append("(  /  )").append("\n").append(" \\(__)|");
+
+		System.out.println(output);
+
+	}
+	
+
+}
