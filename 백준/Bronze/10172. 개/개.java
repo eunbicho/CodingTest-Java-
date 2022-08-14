d1b522a663e6b0362fdd6a5e861cf7308a33a6ae
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-15 00:43:26 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11216 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		output.append("|\\_/|").append("\n").append("|q p|   /}").append("\n").append("( 0 )\"\"\"\\").append("\n").append("|\"^\"`    |").append("\n").append("||_/=\\\\__|");
+
+		System.out.println(output);
+
+	}
+	
+
+}
