0bdcf4ebff7545c6e45bc05a9793a781599e4fc4
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-15 00:35:10 +0900

    [Bronze V] Title: 나머지, Time: 84 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		int A = Integer.parseInt(tokens.nextToken());
+		int B = Integer.parseInt(tokens.nextToken());
+		int C = Integer.parseInt(tokens.nextToken());
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+
+		
+
+	}
+
+}
