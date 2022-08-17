6bed783671309735cf5cc769cb54343040774fc5
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-18 00:25:58 +0900

    Update 쿼드트리.java

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,90 +1,68 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
+
+	static int N;
+	static int[][] image;
 	
-	public static int[][] img;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(input.readLine());
+		image = new int[N][N];
 		
-		int N = Integer.parseInt(br.readLine());
-		img = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			
-			for(int j = 0; j < N; j++) {
-				img[i][j] = str.charAt(j) - '0';
+		for(int r=0; r<N; r++) {
+			String str = input.readLine();
+			for(int c=0; c<N; c++) {
+				image[r][c] = str.charAt(c)-'0';
 			}
-		}
+		} // 입력 끝
 		
-		QuadTree(0, 0, N);
-		System.out.println(sb);
+		Compression(0, 0, N); // 0,0 좌표부터 8x8 안에서 압축 가능한지 탐색 
+		System.out.println(output);
+
 	}
 	
-	public static void QuadTree(int x, int y, int size) {
+	public static void Compression(int r, int c, int size) {
 		
-		// 압축이 가능할 경우 하나의 색상으로 압축해준다.
-		if(isPossible(x, y, size)) {
-			sb.append(img[x][y]);
+		//System.out.println(r +" " + c);
+		// 압축 가능하면 그 색으로 압축!
+		if(isPossible(r, c, size)) {
+			output.append(image[r][c]);
 			return;
 		}
 		
-		int newSize = size / 2;	// 압축이 불가능 할 경우 사이즈를 절반으로 나누어야 한다.
+		// 압축이 불가능하다면
+		int newSize = size/2; // 반으로 나눠서 탐색
 		
-		sb.append('(');	// 각 레벨(depth)에서 여는괄호로 시작해야한다. 
+		output.append('(');
 		
-		QuadTree(x, y, newSize);						// 왼쪽 위
-		QuadTree(x, y + newSize, newSize);				// 오른쪽 위
-		QuadTree(x + newSize, y, newSize);				// 왼쪽 아래
-		QuadTree(x + newSize, y + newSize, newSize);	// 오른쪽 아래
+		Compression(r, c, newSize); // 왼쪽 위
+		Compression(r, c + newSize, newSize); // 오른쪽 위
+		Compression(r + newSize, c, newSize); // 왼쪽 아래
+		Compression(r + newSize, c + newSize, newSize); // 오른쪽 아래
 		
-		sb.append(')');	// 해당 레벨이 끝나면 닫는괄호도 닫아준다.
-			
+		output.append(')');
 		
 	}
 	
 	
-	// 압축이 가능한지 해당 공간을 체크해주는 함수
-	public static boolean isPossible(int x, int y, int size) {
-		int value = img[x][y];
+	public static boolean isPossible(int r, int c, int size) {
+		int color = image[r][c]; // 이 구역의 색 저장
 		
-		for(int i = x; i < x + size; i++) {
-			for(int j = y; j < y + size; j++) {
-				if(value != img[i][j]) {
-					return false;
+		// 이 사이즈 안에 하나라도 다른 값이 있으면 압축 못함
+		for(int i = r; i < r+size; i++) {
+			for(int j=c; j < c+size; j++) {
+				if(color != image[i][j]) {
+					return false; // 색이 다른게 하나라도 생기면 false 반환
 				}
 			}
 		}
+		
 		return true;
 	}
- 
+
 }
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
-
-
-
-
