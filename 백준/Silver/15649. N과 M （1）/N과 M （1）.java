06e063589076bc66506aa8fcdd62f9a10ba254b2
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-26 00:21:46 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 48756 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static int[] src;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		
+		src = new int[N];
+		for(int i=0; i<N; i++) {
+			src[i] = i+1;
+		}
+		
+		perm(0, new int[M], new boolean[N]);
+		System.out.println(output);
+
+	}
+
+	private static void perm(int nth, int[] choosed, boolean[] visited) {
+		if(nth == choosed.length) {
+			for(int i=0; i<choosed.length; i++) {
+				output.append(choosed[i] + " ");
+			}
+			output.append('\n');
+			return;
+		}
+		
+		for(int i=0; i<src.length; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				choosed[nth] = src[i];
+				perm(nth+1, choosed, visited);
+				visited[i] = false;
+			}
+		}
+		
+	}
+
+}
