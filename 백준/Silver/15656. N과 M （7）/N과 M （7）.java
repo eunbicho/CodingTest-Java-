13f6a57b9d07e787a75ca3b66431a5046a613ef3
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-08-28 14:20:26 +0900

    [Silver III] Title: N과 M (7), Time: 616 ms, Memory: 220072 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static int[] nums;
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		nums = new int[N];
+		tokens = new StringTokenizer(input.readLine());
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(tokens.nextToken());
+		}
+		Arrays.sort(nums);
+		perm(0, new int[M]);
+		System.out.println(output);
+
+	}
+
+	private static void perm(int nth, int[] choosed) {
+		if(nth == choosed.length) {
+			for(int i=0; i<choosed.length; i++) {
+				output.append(choosed[i]).append(" ");
+			}
+			output.append('\n');
+			return;
+		}
+		
+		for(int i=0; i<nums.length; i++) {
+							
+				choosed[nth] = nums[i];
+				
+				perm(nth+1, choosed);
+				
+		}
+		
+	}
+
+}
