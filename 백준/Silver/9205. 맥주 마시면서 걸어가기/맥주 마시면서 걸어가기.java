1a100d4a3e8aef96dc05e800e07429336a73a49e
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-10-07 01:22:58 +0900

    [Silver I] Title: 맥주 마시면서 걸어가기, Time: 100 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,104 +1,88 @@
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
- 
-class Point {
-    int x;
-    int y;
- 
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
+	
+	static int T;
+	static int endX, endY = -1;
+	static ArrayList<Point> stores = new ArrayList<>();
+	
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer tokens;
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        
- 
-        int T = Integer.parseInt(input.readLine());
- 
-        ArrayList<Point> a; // 집, 편의점, 페스티벌에 위치를 저장하는 배열
-        ArrayList<ArrayList<Integer>> graph;
-        
-        StringBuilder output = new StringBuilder();
-        while (T-- > 0) {
-            int N = Integer.parseInt(input.readLine());
- 
-            a = new ArrayList<>();
-            
-            // 집, 편의점, 페스티벌에 위치를 저장함.
-            for (int i = 0; i < N + 2; i++) {
-                tokens = new StringTokenizer(input.readLine());
-                int x = Integer.parseInt(tokens.nextToken());
-                int y = Integer.parseInt(tokens.nextToken());
- 
-                a.add(new Point(x, y));
-            }
- 
-            graph = new ArrayList<>();
-            for (int i = 0; i < N + 2; i++) {
-                graph.add(new ArrayList<>());
-            }
-            
-            // 맨해튼 거리 1000m 이하를 만족하는 두 정점을 찾음.
-            // 그리고 양방향 그래프로 서로 이어 줌.
-            for (int i = 0; i < N + 2; i++) {
-                for (int j = i + 1; j < N + 2; j++) {
-                    if (calDistance(a.get(i), a.get(j)) <= 1000) {
-                        graph.get(i).add(j);
-                        graph.get(j).add(i);
-                    }
-                }
-            }
-            
-            // BFS를 이용하여 집에서 페스티벌까지 갈 수 있는지 탐색.
-            output.append((BFS(graph, N) ? "happy" : "sad") + '\n');
-        }
-        
-        System.out.println(output);
-    }
-    
-    // 맨해튼 거리
-    public static int calDistance(Point p1, Point p2) {
-        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
-    }
-    
-    // 전형적인 BFS 코드
-    public static boolean BFS(ArrayList<ArrayList<Integer>> graph, int N) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
- 
-        boolean[] visited = new boolean[N + 2];
-        visited[0] = true;
- 
-        while (!q.isEmpty()) {
-            int now = q.poll();
- 
-            if (now == N + 1) {
-                return true;
-            }
- 
-            for (int next : graph.get(now)) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
-        }
- 
-        return false;
-    }
- 
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws Exception {
+		T = Integer.parseInt(input.readLine());
+		for(int i=0; i<T; i++) {
+			stores.clear(); // 테케마다 상점 배열 초기화
+			int n = Integer.parseInt(input.readLine());
+			tokens = new StringTokenizer(input.readLine());
+			
+			// 시작 위치 저장
+			int startX = Integer.parseInt(tokens.nextToken());
+			int startY = Integer.parseInt(tokens.nextToken());
+			
+			// 편의점 위치 저장
+			for(int j=0; j<n; j++) {
+				tokens = new StringTokenizer(input.readLine());
+				stores.add(new Point(Integer.parseInt(tokens.nextToken()),Integer.parseInt(tokens.nextToken())));
+			}
+			
+			tokens = new StringTokenizer(input.readLine());
+			
+			// 도착 위치 저장
+			endX = Integer.parseInt(tokens.nextToken());
+			endY = Integer.parseInt(tokens.nextToken());
+			
+			// bfs 반환 결과에 따라 happy, sad 출력
+			if(bfs(startX, startY)) {
+				output.append("happy"+"\n");
+			} else {
+				output.append("sad"+"\n");
+			}
+	
+		}
+		System.out.println(output);
+	}
+	
+	
+	private static boolean bfs(int startX, int startY) {
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(startX,startY)); // 출발점 먼저 넣기
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			while(size-- > 0) {				
+				Point head = q.poll();
+				
+				// 만약 현재 좌표에서부터 도착지점까지의 거리가 1000이하라면? -> 갈 수 있음! happy~
+				if(Math.abs(head.x - endX) + Math.abs(head.y - endY) <= 1000)
+					return true;
+				
+				// 아니라면 현재 좌표에서부터 거리가 1000이하인 편의점들을 큐에 넣어주고 리스트에서 지우기
+				for(int j=0; j<stores.size(); j++) {
+					if(Math.abs(stores.get(j).x - head.x) + Math.abs(stores.get(j).y - head.y) <=1000) {
+						q.add(new Point(stores.get(j).x,stores.get(j).y));
+						stores.remove(j);
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+
+	static class Point {
+	    int x;
+	    int y;
+	 
+	    Point(int x, int y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+	}
+
 }
