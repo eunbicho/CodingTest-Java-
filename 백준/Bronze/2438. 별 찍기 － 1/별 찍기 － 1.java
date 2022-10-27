0ff9cf80632bae9d6bcd293d6e8b591e38b33bb1
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-10-27 09:38:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 168 ms, Memory: 12660 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(input.readLine());
+		for(int i=1; i<=N; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
