7f183fb05f98a3cc2b82d93044bebce41165ed11
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2022-12-04 16:56:47 +0900

    [Bronze V] Title: 콜센터, Time: 68 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	static StringBuilder output = new StringBuilder();
+	public static void main(String[] args) {
+	 output.append("     /~\\").append("\n");
+	 output.append("    ( oo|").append("\n");
+	 output.append("    _\\=/_").append("\n");
+	 output.append("   /  _  \\").append("\n");
+	 output.append("  //|/.\\|\\\\").append("\n");
+	 output.append(" ||  \\ /  ||").append("\n");
+	 output.append("============").append("\n");
+	 for(int i=0; i<3; i++) {		 
+		 output.append("|          |").append("\n");
+	 }
+	 System.out.println(output);
+
+	}
+
+}
