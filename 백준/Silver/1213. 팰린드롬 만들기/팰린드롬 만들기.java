4827579c0b4e100a72efc6c18b38c29c98c3cfae
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2023-01-03 21:31:10 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 80 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,83 +1,61 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-
+	
 	static BufferedReader input = new BufferedReader((new InputStreamReader(System.in)));
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		
-		char[] name  = input.readLine().toCharArray();
-		HashMap<Character, Integer> map = new HashMap<>();
+		// 입력받기
+		String name = input.readLine();
+		int len = name.length();
 		
-		// 1. 어떤 알파벳이 몇 개 있는지 파악
-		for(int i=0; i<name.length; i++) {
-			int count = 1;
-			if(map.containsKey(name[i])) {
-				count = map.get(name[i])+1;
-			}
-			map.put(name[i], count);
+		// a부터 z까지 총 26개의 알파벳 개수를 셀 count 배열이다.
+		int[] count = new int[26];
+		
+		// 알파벳 개수를 count배열에 저장한다. 
+		// 'A'-'A' = 0, 'B'-'A' = 1 ... 을 이용한 코드
+		for(int i = 0; i < len; i++) {
+			count[name.charAt(i)-'A']++;
 		}
-
 		
-		// 2. 홀수개가 2개 이상이면 쏘리 출력
+		// 홀수 개수를 셀 cnt (내 코드에서는 oddCount)
+		// 홀수 개수인 알파벳의 인덱스를 나타내는 oddIdx
+		int cnt = 0, oddIdx = 0;
 		
-		int oddCount = 0;
-		char oddChar = 'a';
-		
-		for (Map.Entry<Character, Integer> pair : map.entrySet()) {
-			if(pair.getValue() % 2 == 1) {
-				oddCount++;
-				oddChar = pair.getKey();
+		for(int i = 0; i < 26; i++) {
+			if(count[i] % 2 == 1) { // 홀수 개수가 있으면 cnt를 올리고 그 알파벳 인덱스를 저장해놓는다.
+				cnt++;
+				oddIdx = i;
 			}
-			if(oddCount >=2 ) {
-				System.out.println("I'm Sorry Hansoo");
-				break;
-			}
-		
 		}
-
-		// 3. 홀수개가 1개이하라면 2로 나눈 개수만큼 문자열 만들기
-		if(oddCount <=1 ) {
-			
-			StringBuilder half = new StringBuilder();
-			for (Map.Entry<Character, Integer> pair : map.entrySet()) {
-				for(int i=0; i<pair.getValue()/2; i++) {
-					half.append(pair.getKey());
-				}
-			}
-			
-			
-			// 4. 사전순으로 정렬
-			char[] chars = half.toString().toCharArray();
-			Arrays.sort(chars);
-			
-			half.delete(0, half.length());
-			for(int i=0; i<chars.length; i++) {
-				half.append(chars[i]);
-			}
-			
-			StringBuilder ans = new StringBuilder(half.toString());
-			
-			// 5. 홀수 있었으면 1개 붙이고
-			if(oddCount == 1) {
-				
-				ans.append(oddChar);
-			}
-			
-			// 6. 정렬한거 거꾸로 해서 붙여서 출력하기
-			half.reverse();
-			ans.append(half);
-			
-			// 7. 답 출력하기
-			System.out.println(ans);
-			
+		
+		// 홀수 개수가 1이상이면 팰린드롬을 만들지 못한다.
+		if(cnt > 1) {
+			System.out.println("I'm Sorry Hansoo");
+			return;
 		}
-
+		
+		
+		int idx = 0;
+		char[] ans = new char[len];
+		
+		for(int i = 0; i < 26; i++) {
+			// 개수가 2 이상인 것들 (짝수개이고, 남아있는 개수가 있다면 순서대로 넣는다. 대칭이 되게!)
+			// (char)65 = 'A'
+			while(count[i] >= 2) { 
+				ans[idx] = ans[len-1-idx] = (char)(65+i);
+				idx++;
+				count[i] -= 2;
+			}
+		}
+		
+		// 홀수개인 알파벳을 알맞은 자리에 (가운데에) 넣어준다.
+		if(cnt == 1) ans[idx] = (char) (65+oddIdx);
+		
+		// 정답 출력
+		System.out.println(new String(ans));
 	}
-
 }
