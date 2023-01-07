d3f3eab138244b2260722904ae9754677b3c24cd
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2023-01-07 23:12:21 +0900

    [Silver I] Title: 튜터-튜티 관계의 수, Time: 720 ms, Memory: 96388 KB -BaekjoonHub

--- 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
+++ 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
@@ -2,76 +2,68 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M;
+	static int N, M, count;
 	static long ans = 1;
 	static List[] graph;
 	static boolean[] visited;
 
+	
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
-
-	public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws Exception{
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
-
-		graph = new List[N + 1];
-		for (int i = 1; i <= N; i++) {
+		
+		graph = new List[N+1];
+		for(int i=1; i<=N; i++) {
 			graph[i] = new ArrayList<>();
 		}
-		visited = new boolean[N + 1];
-
-		for (int i = 0; i < M; i++) {
+		visited = new boolean[N+1];
+		
+		
+		for(int i=0; i<M; i++) {
 			tokens = new StringTokenizer(input.readLine());
 			int a = Integer.parseInt(tokens.nextToken());
 			int b = Integer.parseInt(tokens.nextToken());
 			graph[a].add(b);
 			graph[b].add(a);
 
-		}
-
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
-				ans = (ans * bfs(i))%1000000007;
-			}
+			
 		}
 		
+		for(int i=1; i<=N; i++) {
+			if(!visited[i]) {
+				dfs(i);
+				if(count !=0) {
+					ans = (ans * count) % 1000000007;	
+				}
+				count = 0;
+			}
+		}
 		System.out.println(ans);
+		
 
 	}
 
-	private static int bfs(int v) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(v);
+	private static void dfs(int v) {
 		visited[v] = true;
-		int count = 1;
+		count++;
 		
-		while (!q.isEmpty()) {
-			int size = q.size();
-			while (size-- > 0) {
-				Integer head = q.poll();
-				
-				//System.out.println(head + " " +count);
-				List<Integer> childs = graph[head];
-				for (Integer child : childs) {
-					if (!visited[child]) {				
-						q.offer(child);
-						visited[child] = true;
-						count += 1;
-					}
-				}
+		List<Integer> childs = graph[v];
+		for(Integer child : childs) {
+			if(!visited[child]) {
+				dfs(child);
 			}
 		}
 		
-		return count;
-
 	}
 
 }
