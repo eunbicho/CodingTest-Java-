0ad1b02c1ba8110654dfd2dc6ac92de5466621db
Eunbi Cho <36323800+eunbicho@users.noreply.github.com>
2023-03-18 20:49:37 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+
+	static int n;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception{
+		n = Integer.parseInt(input.readLine());
+		System.out.println(n);
+		System.out.println(1);
+
+	}
+
+}
